f640355e70be51cb294f3aa687de4dd33572b3fd
CbdFocus <baodong.cao2@wework.com>
2019-01-15 23:44:06 +0800

    cap

---
+++ config/deploy/production.rb
@@ -0,0 +1,13 @@
+server_list = %w(root@172.96.194.92:28897)
+
+set :rails_env, :production
+
+role :puma_nginx, server_list
+role :app, server_list
+role :web, server_list
+# role :db,  %w{deploy@example.com}
+
+set :puma_env, fetch(:rails_env)
+set :puma_threads, [1, 2]
+
+set :nginx_server_name, "www.secretube.tk secretube.tk v1.secretube.tk"

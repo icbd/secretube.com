30f659f662d43ee46d23c46f2e1e2a26610e5dbe
CbdFocus <baodong.cao2@wework.com>
2019-01-24 13:57:19 +0800

    add mysql prod conf

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  # soft delete
+  acts_as_paranoid
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,12 @@
 server_list = %w(root@172.96.194.92:28897)
+mysql_list = %w(root@172.96.194.92:28897)
 
 set :rails_env, :production
 
 role :puma_nginx, server_list
 role :app, server_list
 role :web, server_list
-# role :db,  %w{deploy@example.com}
+role :db,  mysql_list
 
 set :puma_env, fetch(:rails_env)
 set :puma_threads, [1, 2]

2b14c3cb5dfad7f8e662f2bdbd879643b9d708c3
CbdFocus <baodong.cao2@wework.com>
2019-02-08 19:10:51 +0800

    price page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,20 +2,20 @@ class UsersController < ApplicationController
   # POST
   # /users
   def create
-    user = User.new(safe_params)
+    user = User.new(safe_create_params)
     if user.save
       flash[:success] = I18n.t('welcome_come')
       # TODO: SEND EMAIL
       redirect_to dashboard_url
     else
       flash[:warning] = user.errors.full_messages
-      redirect_to register_url
+      redirect_to price_url
     end
   end
 
   private
 
-  def safe_params
+  def safe_create_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   end
 
   get 'dashboard', to: 'dashboard#index'
+  get 'price', to: 'dashboard#price'
 end
